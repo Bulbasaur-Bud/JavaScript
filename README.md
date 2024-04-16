d8de420ea89e0c95a526afab7c2b2754f3ac3b92
Praphan <praphanhc@gmail.com>
2024-04-16 22:46:14 +0800

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # JavaScript
+
 JavaScript from Codecademy
+
+Please be kind. To anyone. Even starbuck workers. They need to stand for a whole day, to serve every single customer.
+You might not know how they feel. They wanted to be same as you, sit and relax on the couch in the starbuck.
+Enjoy your day Starbuck. -some tot from Praphan which you might not agree with
--- class_day_01.js
+++ class_day_01.js
@@ -538,120 +538,246 @@ Write a function groceries() that takes an array of object literals of grocery i
 
 //Credit Card Checker
 
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+// // All valid credit card numbers
+// const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+// const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+// const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+// const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+// const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+// // All invalid credit card numbers
+// const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+// const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+// const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+// const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+// const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
 
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+// // Can be either valid or invalid
+// const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+// const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+// const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+// const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+// const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
 
-// An array of all the arrays above
-const batch = [
-  valid1,
-  valid2,
-  valid3,
-  valid4,
-  valid5,
-  invalid1,
-  invalid2,
-  invalid3,
-  invalid4,
-  invalid5,
-  mystery1,
-  mystery2,
-  mystery3,
-  mystery4,
-  mystery5,
-];
+// // An array of all the arrays above
+// const batch = [
+//   valid1,
+//   valid2,
+//   valid3,
+//   valid4,
+//   valid5,
+//   invalid1,
+//   invalid2,
+//   invalid3,
+//   invalid4,
+//   invalid5,
+//   mystery1,
+//   mystery2,
+//   mystery3,
+//   mystery4,
+//   mystery5,
+// ];
 
-// Add your functions below:
-// Add your functions below:
-function validateCred(numArr) {
-  let total = 0;
-  for (let i = numArr.length - 1; i >= 0; i--) {
-    let currValue = numArr[i];
-    if ((numArr.length - 1 - i) % 2 === 1) {
-      currValue *= 2;
-      if (currValue > 9) {
-        currValue -= 9;
-      }
-    }
-    total += currValue;
+// // Add your functions below:
+// // Add your functions below:
+// function validateCred(numArr) {
+//   let total = 0;
+//   for (let i = numArr.length - 1; i >= 0; i--) {
+//     let currValue = numArr[i];
+//     if ((numArr.length - 1 - i) % 2 === 1) {
+//       currValue *= 2;
+//       if (currValue > 9) {
+//         currValue -= 9;
+//       }
+//     }
+//     total += currValue;
+//   }
+
+//   return total % 10 === 0;
+// }
+
+// // Test functions:
+// console.log(validateCred(valid1)); // Should print true
+// console.log(validateCred(invalid1)); // Should print false
+
+// function findInvalidCards(cards) {
+//   const invalid = [];
+
+//   for (let i = 0; i < cards.length; i++) {
+//     let currCred = cards[i];
+//     if (!validateCred(currCred)) {
+//       invalid.push(currCred);
+//     }
+//   }
+
+//   return invalid;
+// }
+
+// // Test function
+// console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5])); // Shouldn't print anything
+// console.log(
+//   findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])
+// ); // Should print all of the numbers
+
+// console.log(findInvalidCards(batch)); // Test what the mystery numbers are
+
+// function idInvalidCardCompanies(invalidBatch) {
+//   const companies = [];
+//   for (let i = 0; i < invalidBatch.length; i++) {
+//     switch (invalidBatch[i][0]) {
+//       case 3:
+//         if (companies.indexOf("Amex") === -1) {
+//           companies.push("Amex");
+//         }
+//         break;
+//       case 4:
+//         if (companies.indexOf("Visa") === -1) {
+//           companies.push("Visa");
+//         }
+//         break;
+//       case 5:
+//         if (companies.indexOf("Mastercard") === -1) {
+//           companies.push("Mastercard");
+//         }
+//         break;
+//       case 6:
+//         if (companies.indexOf("Discover") === -1) {
+//           companies.push("Discover");
+//         }
+//         break;
+//       default:
+//         console.log("Company not found");
+//     }
+//   }
+//   return companies;
+// }
+
+// console.log(idInvalidCardCompanies([invalid1])); // Should print['visa']
+// console.log(idInvalidCardCompanies([invalid2])); // Should print ['mastercard']
+// console.log(idInvalidCardCompanies(batch)); // Find out which companies have mailed out invalid cards
+
+// class Dog {
+//   constructor(name) {
+//     this._name = name;
+//     this._behavior = 5;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+//   get behavior() {
+//     return this._behavior;
+//   }
+
+//   incrementBehavior() {
+//     this._behavior++;
+//   }
+// }
+
+// const halley = new Dog("Halley");
+// console.log(halley.name); // Print name value to console
+// console.log(halley.behavior); // Print behavior value to console
+// halley.incrementBehavior(); // Add one to behavior
+// console.log(halley.name); // Print name value to console
+// console.log(halley.behavior); // Print behavior value to console
+// halley.incrementBehavior(); // Add one to behavior
+// console.log(halley.name); // Print name value to console
+// console.log(halley.behavior);
+
+// const milo = new Dog("Milo");
+// console.log(milo.name); // Print name value to console
+// console.log(milo.behavior); // Print behavior value to console
+// milo.incrementBehavior(); // Add one to behavior
+// console.log(milo.name); // Print name value to console
+// console.log(milo.behavior); // Print behavior value to console
+// milo.incrementBehavior(); // Add one to behavior
+// console.log(milo.name); // Print name value to console
+// console.log(milo.behavior);
+
+//Method Call
+// class Surgeon {
+//   constructor(name, department) {
+//     this._name = name;
+//     this._department = department;
+//     this._remainingVacationDays = 20;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   get department() {
+//     return this._department;
+//   }
+
+//   get remainingVacationDays() {
+//     return this._remainingVacationDays;
+//   }
+
+//   takeVacationDays(daysOff) {
+//     this._remainingVacationDays -= daysOff;
+//   }
+// }
+
+// const surgeonRomero = new Surgeon("Francisco Romero", "Cardiovascular");
+// const surgeonJackson = new Surgeon("Ruth Jackson", "Orthopedics");
+
+// console.log(surgeonRomero.name);
+// surgeonRomero.takeVacationDays(3);
+// console.log(surgeonRomero.remainingVacationDays);
+
+//Inheritance II
+
+// class HospitalEmployee {
+//   constructor(name) {
+//     this._name = name;
+//     this._remainingVacationDays = 20;
+//   }
+
+//   get name() {
+//     return this._name;
+//   }
+
+//   get remainingVacationDays() {
+//     return this._remainingVacationDays;
+//   }
+
+//   takeVacationDays(daysOff) {
+//     this._remainingVacationDays -= daysOff;
+//   }
+// }
+
+// const doctorJackson = new HospitalEmployee("Jackson", 18);
+// console.log(doctorJackson);
+// console.log(HospitalEmployee.name);
+
+//Inheritance III
+
+class Animal {
+  constructor(name) {
+    this._name = name;
+    this._behavior = 0;
   }
 
-  return total % 10 === 0;
-}
-
-// Test functions:
-console.log(validateCred(valid1)); // Should print true
-console.log(validateCred(invalid1)); // Should print false
-
-function findInvalidCards(cards) {
-  const invalid = [];
-
-  for (let i = 0; i < cards.length; i++) {
-    let currCred = cards[i];
-    if (!validateCred(currCred)) {
-      invalid.push(currCred);
-    }
+  get name() {
+    return this._name;
   }
 
-  return invalid;
-}
-
-// Test function
-console.log(findInvalidCards([valid1, valid2, valid3, valid4, valid5])); // Shouldn't print anything
-console.log(
-  findInvalidCards([invalid1, invalid2, invalid3, invalid4, invalid5])
-); // Should print all of the numbers
-
-console.log(findInvalidCards(batch)); // Test what the mystery numbers are
-
-function idInvalidCardCompanies(invalidBatch) {
-  const companies = [];
-  for (let i = 0; i < invalidBatch.length; i++) {
-    switch (invalidBatch[i][0]) {
-      case 3:
-        if (companies.indexOf("Amex") === -1) {
-          companies.push("Amex");
-        }
-        break;
-      case 4:
-        if (companies.indexOf("Visa") === -1) {
-          companies.push("Visa");
-        }
-        break;
-      case 5:
-        if (companies.indexOf("Mastercard") === -1) {
-          companies.push("Mastercard");
-        }
-        break;
-      case 6:
-        if (companies.indexOf("Discover") === -1) {
-          companies.push("Discover");
-        }
-        break;
-      default:
-        console.log("Company not found");
-    }
+  get behavior() {
+    return this._behavior;
   }
-  return companies;
+
+  incrementBehavior() {
+    this._behavior++;
+  }
 }
 
-console.log(idInvalidCardCompanies([invalid1])); // Should print['visa']
-console.log(idInvalidCardCompanies([invalid2])); // Should print ['mastercard']
-console.log(idInvalidCardCompanies(batch)); // Find out which companies have mailed out invalid cards
+class Cat extends Animal {
+  constructor(name, usesLitter) {
+    super(name);
+    this._usesLitter = usesLitter;
+  }
+}
+
+const bryceCat = new Cat("Bryce", false);
+console.log(bryceCat._name); // output: Bryce
