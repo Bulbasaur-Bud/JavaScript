2a042831f5a2196a87d79ddb959a475211ab412f
Praphan <praphanhc@gmail.com>
2024-04-27 17:25:31 +0800

    eighth commit

--- class_day_01.js
+++ class_day_01.js
@@ -1100,40 +1100,116 @@ from Animal class.
 
 //Async & Await
 
-const fs = require("fs");
-const promisifiedReadfile = require("./promisifiedReadfile");
+// const fs = require("fs");
+// const promisifiedReadfile = require("./promisifiedReadfile");
 
-// Here we use fs.readfile() and callback functions:
-fs.readFile("./file.txt", "utf-8", (err, data) => {
-  if (err) throw err;
-  let firstSentence = data;
-  fs.readFile("./file2.txt", "utf-8", (err, data) => {
-    if (err) throw err;
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence);
-  });
-});
+// // Here we use fs.readfile() and callback functions:
+// fs.readFile("./file.txt", "utf-8", (err, data) => {
+//   if (err) throw err;
+//   let firstSentence = data;
+//   fs.readFile("./file2.txt", "utf-8", (err, data) => {
+//     if (err) throw err;
+//     let secondSentence = data;
+//     console.log(firstSentence, secondSentence);
+//   });
+// });
 
-// Here we use native promises with our "promisified" version of readfile:
-let firstSentence;
-promisifiedReadfile("./file.txt", "utf-8")
-  .then((data) => {
-    firstSentence = data;
-    return promisifiedReadfile("./file2.txt", "utf-8");
-  })
-  .then((data) => {
-    let secondSentence = data;
-    console.log(firstSentence, secondSentence);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// // Here we use native promises with our "promisified" version of readfile:
+// let firstSentence;
+// promisifiedReadfile("./file.txt", "utf-8")
+//   .then((data) => {
+//     firstSentence = data;
+//     return promisifiedReadfile("./file2.txt", "utf-8");
+//   })
+//   .then((data) => {
+//     let secondSentence = data;
+//     console.log(firstSentence, secondSentence);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-// Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
-async function readFiles() {
-  let firstSentence = await promisifiedReadfile("./file.txt", "utf-8");
-  let secondSentence = await promisifiedReadfile("./file2.txt", "utf-8");
-  console.log(firstSentence, secondSentence);
+// // Here we use promisifiedReadfile() again but instead of using the native promise .then() syntax, we declare and invoke an async/await function:
+// async function readFiles() {
+//   let firstSentence = await promisifiedReadfile("./file.txt", "utf-8");
+//   let secondSentence = await promisifiedReadfile("./file2.txt", "utf-8");
+//   console.log(firstSentence, secondSentence);
+// }
+
+// readFiles();
+
+//Async & Await
+
+// async function fivePromise() {
+//   return 5;
+// }
+
+// fivePromise().then((resolvedValue) => {
+//   console.log(resolvedValue);
+// }); // Prints 5
+
+// function withConstructor(num) {
+//   return new Promise((resolve, reject) => {
+//     if (num === 0) {
+//       resolve("zero");
+//     } else {
+//       resolve("not zero");
+//     }
+//   });
+// }
+
+// withConstructor(0).then((resolveValue) => {
+//   console.log(
+//     ` withConstructor(0) returned a promise which resolved to: ${resolveValue}.`
+//   );
+// });
+
+// // Write your code below:
+// async function withAsync(num) {
+//   if (num === 0) {
+//     return "zero";
+//   } else {
+//     return "not zero";
+//   }
+// }
+
+// withAsync(100).then((resolveValue) => {
+//   console.log(
+//     ` withAsync(100) returned a promise which resolved to: ${resolveValue}.`
+//   );
+// });
+
+// async function asyncFuncExample() {
+//   let resolvedValue = await myPromise();
+//   console.log(resolvedValue);
+// }
+
+// asyncFuncExample(); // Prints: I am resolved now!
+
+// const brainstormDinner = require("./library.js");
+
+// // Native promise version:
+// function nativePromiseDinner() {
+//   brainstormDinner().then((meal) => {
+//     console.log(`I'm going to make ${meal} for dinner.`);
+//   });
+// }
+
+// // async/await version:
+// async function announceDinner() {
+//   // Write your code below:
+//   let meal = await brainstormDinner();
+//   console.log(`I'm going to make ${meal} for dinner.`);
+// }
+
+// announceDinner();
+
+const shopForBeans = require("./library.js");
+
+async function getBeans() {
+  console.log(`1. Heading to the store to buy beans...`);
+  let value = await shopForBeans();
+  console.log(`3. Great! I'm making ${value} beans for dinner tonight!`);
 }
 
-readFiles();
+getBeans();
